174236bc18e12d9478e1ad7bae687371022e0c2c
Yuriy Gorvitovskiy <yuriy@gorvitovskiy.com>
2018-08-27 00:09:00 -0700

    Adjust SQL file to match default parameters

--- sql/postgres/create-db.sql
+++ sql/postgres/create-db.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE state_patch_sql;
-CREATE USER state_patch_sql WITH ENCRYPTED PASSWORD 'xxxxxxx';
+CREATE USER state_patch_sql WITH ENCRYPTED PASSWORD 'secret';
 GRANT ALL PRIVILEGES ON DATABASE state_patch_sql TO state_patch_sql;
